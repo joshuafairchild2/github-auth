35133d3f99d6cc73c6e04407c507d2ffd8a4510c
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-07-18 11:11:24 -0700

    implement basic login system using firebase w/ github authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from './authentication.service';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AuthenticationService]
 })
 export class AppComponent {
-  title = 'app works!';
+  private user: Observable<firebase.User>;
+  private isLoggedIn: boolean;
+  private displayName: string;
+
+  constructor(
+    public authService: AuthenticationService
+  ) {
+    this.authService.user.subscribe(user => {
+      if (user == null) {
+        this.isLoggedIn = false;
+      } else {
+        this.isLoggedIn = true;
+        this.displayName = user.displayName;
+      }
+    });
+  }
+
+  login(): void {
+    this.authService.login();
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
 }
---
+++ src/app/authentication.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
+@Injectable()
+export class AuthenticationService {
+  user: Observable<firebase.User>;
+
+  constructor(
+    public ngFireAuth: AngularFireAuth
+  ) {
+    this.user = ngFireAuth.authState;
+  }
+
+  login(): void {
+    this.ngFireAuth.auth.signInWithPopup(new firebase.auth.GithubAuthProvider());
+  }
+
+  logout(): void {
+    this.ngFireAuth.auth.signOut();
+  }
+
+}
